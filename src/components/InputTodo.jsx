b93ba426eed636e2aa17c47d8cc1e236f170b1ae
gamino33 <standard3310@gmail.com>
2021-06-14 16:00:58 +0900

    init

---
+++ src/components/InputTodo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const InputTodo = () => {
+    return (
+        <>
+         <h1>Hello</h1>
+        </>
+    );
+};
+
+export default InputTodo;
---
+++ src/components/ListTodos.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ListTodo = () => {
+    return (
+        <>
+         <h1>Hello</h1>
+        </>
+    );
+};
+
+export default ListTodo;
