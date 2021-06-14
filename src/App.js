688303be5750f6233fb69c27f639003da0aa415f
gamino33 <standard3310@gmail.com>
2021-06-15 01:25:55 +0900

    TodoLIstを表示

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import InputTodo from './components/InputTodo';
+import ListTodos from './components/ListTodos';
 import Layout from './components/Layout';
 
 function App() {
   return (
     <Layout>
         <InputTodo />
+        <ListTodos />
     </Layout>
   );
 }
--- src/components/ListTodos.jsx
+++ src/components/ListTodos.jsx
@@ -1,11 +1,83 @@
-import React from "react";
+import React, {useState, useEffect} from "react"
+import TableContainer from "@material-ui/core/TableContainer"
+import Table from "@material-ui/core/Table"
+import TableBody from "@material-ui/core/TableBody"
+import TableCell from "@material-ui/core/TableCell"
+import TableRow from "@material-ui/core/TableRow"
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
+import IconButton from "@material-ui/core/IconButton"
+import {makeStyles} from "@material-ui/styles"
 
-const ListTodo = () => {
+const useStyles = makeStyles({
+    iconCell: {
+        padding: 0,
+        height: 48,
+        width: 48
+    }
+})
+
+const ListTodos = () => {
+
+    const [todos, setTodos] = useState([]);
+    const classes = useStyles();
+
+    const deleteTodo = async(id) => {
+        try {
+            const deleteTodo = await fetch(`https://pern-todo33.herokuapp.com/todos/${id}`, {
+                method: "DELETE"
+            });
+            setTodos(todos.filter(todo => todo.todo_id !== id));
+        } catch (err) {
+            console.error(err.message);
+        }
+    };
+
+    const getTodos = async() => {
+        try {
+            const response = await fetch("https://pern-todo33.herokuapp.com/todos")
+            const jsonData = await response.json();
+            setTodos(jsonData);
+        } catch (err) {
+            console.error(err.messsage);
+        }
+    };
+
+    useEffect(() => {
+        getTodos();
+    }, []);
+
     return (
-        <>
-         <h1>Hello</h1>
-        </>
-    );
-};
+        <TableContainer>
+            <h2>TODO LIST</h2>
+            <Table>
+                <TableBody>
+                    {todos.length > 0 && (
+                        todos.map( todo => (
+                            <TableRow key={todo.todo_id}>
+                                <TableCell>
+                                    <h2>{todo.description}</h2>
+                                </TableCell>
+                                <TableCell className={classes.iconCell}>
+                                    <IconButton >
+                                        <EditIcon />
+                                    </IconButton>
+                                </TableCell>
+                                <TableCell className={classes.iconCell}>
+                                    <IconButton  onClick={() => deleteTodo(todo.todo_id)}>
+                                        <DeleteIcon />
+                                    </IconButton>
+                                </TableCell>
+                            </TableRow>
+                        ))
+                    )}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    )
 
-export default ListTodo;
+
+
+}
+
+export default ListTodos;
